ded214067cc2f8a3bef31d072fcc43fba84778c1
devpalace <36732944+devpalace@users.noreply.github.com>
2021-08-29 11:35:30 +0000

    Deploying to main from @ devpalace/invoice-generator@5ae9c8cd4b73f4d39c50ac953306a574748443aa 🚀

--- src/components/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <v-footer color="indigo" app>
-    <span class="white--text"
-      >&copy; {{ new Date().getFullYear() }} Designed & Developed By Team</span
-    >
-  </v-footer>
-</template>
