a9daa0b837aacfa237030a749404ba21bb0aa503
Altaf <khan.blpr@gmail.com>
2021-08-29 17:44:51 +0530

    changed path

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer color="indigo" app>
     <span class="white--text"
-      >&copy; {{ new Date().getFullYear() }} Designed & Developed By Team</span
+      >&copy; {{ new Date().getFullYear() }} Designed & Developed By https://github.com/devpalace</span
     >
   </v-footer>
 </template>
