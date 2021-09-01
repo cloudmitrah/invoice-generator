8a3bfd8a228a1bb386a34a8e963205a1d7c70f88
Altaf <khan.blpr@gmail.com>
2021-09-01 10:03:42 +0530

    changed footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer color="indigo" app>
     <span class="white--text"
-      >&copy; {{ new Date().getFullYear() }} Designed with <span style="color: #e25555;">&#9829;</span> Developed By <a href="https://github.com/devpalace" target="_blank" style="color:#FF5252;">  devpalace</a> </span
+      >&copy; {{ new Date().getFullYear() }} Designed with <span style="color: #e25555;">&#9829;</span> Developed By <a href="https://github.com/cloudmitrah" target="_blank" style="color:#FF5252;">  cloudmitrah</a> </span
     >
   </v-footer>
 </template>
